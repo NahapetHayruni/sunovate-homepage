af24feb495bb6cd67208a3509f0a0c15cb3a6177
NahapetHayruni <nahapet_h@yahoo.com>
2025-08-13 15:31:52 +0400

    Implemented "coming soon"

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming soon.
         </p>
-        <a
+	<a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	  href="mailto:support@sunovate.am?bcc=nahapet@sunovate.solar"
+	>
+	  support@sunovate.am
+	</a>
       </header>
     </div>
   );
